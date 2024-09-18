51f695e7a29ae1b0b06ed4dd8da534b9a075f12d
mtalarek <82826461+mtalarek@users.noreply.github.com>
2024-09-18 22:04:38 +0200

    Create zadanie4.2.cpp

---
+++ zadanie4.2.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+
+int zlicz_cyfry(long long a) {
+	int licznik_cyfr = 0;
+	if (a == 0) {
+		licznik_cyfr = 1;
+	}
+	else {
+		if (a < 0) {
+			a = -a;
+		}
+		licznik_cyfr = 0;
+		while (a > 0) {
+			a = a / 10;
+			licznik_cyfr++;
+		}
+	}
+	return licznik_cyfr;
+}
+
+int main() {
+	long long a;
+	cout << "podaj liczbe: ";
+	cin >> a;
+	cout << "liczba cyfr w " << a << " = " << zlicz_cyfry(a);
+}
